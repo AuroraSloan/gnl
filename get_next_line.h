afcc1832423c18bf251d15dab851f036b694fbcf
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 18:20:39 +0900

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -5,7 +5,7 @@
 
 int	get_next_line(int fd, char **line);
 size_t	ft_strlen(char *s);
-size_t	ft_strchr_len(char *s, int c); 
+size_t	ft_strchr_len(char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *str);
 char	*ft_substr(char *s, unsigned int start, size_t len);
