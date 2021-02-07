6e914cdde8152f2921c4f0d4c477395a65909cce
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 21:54:15 +0900

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,11 +3,11 @@
 
 # include <stdlib.h>
 
-int	get_next_line(int fd, char **line);
-size_t	ft_strlen(char *s);
-size_t	ft_strchr_len(char *s, int c);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strdup(char *s1);
-char	*ft_substr(char *s, unsigned int start, size_t len);
+int		get_next_line(int fd, char **line);
+size_t		ft_strlen(char *s);
+size_t		ft_strchr_len(char *s, int c);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strdup(char *s1);
+char		*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
