390a318247a9847f60884f1d885f6783c57a97f6
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 18:09:30 +0900

    Create get_next_line_utils.c

---
+++ get_next_line_utils.c
@@ -0,0 +1,93 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr_len(const char *s, int c)
+{
+	char	*ret;
+
+	if(s[0] == (char)c)
+		return (1);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dst;
+	int	i;
+
+	if (!(dst = (char*)malloc(sizeof(*dst) * ft_strlen(s1) + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int	i;
+	int	j;
+	int	len;
+	char	*join;
+
+	if (!s1 || !s2)
+		return ((char*)s1);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	if (!(join = (char *)malloc(sizeof(*join) * len + 1)))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		join[i + j] = s2[j];
+		j++;
+	}
+	join[i + j] = '\0';
+	return (join);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char		*sub_s;
+
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (!(sub_s = (char*)malloc(sizeof(*sub_s) * len + 1)))
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		sub_s[i] = s[start + i];
+		i++;
+	}
+	sub_s[i] = '\0';
+	return (sub_s);
+}
