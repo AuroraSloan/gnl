47a77be96b70239c4152ba861b416775a5b49f2f
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-07 18:01:23 +0900

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+size_t	ft_strlen(char *s);
+size_t	ft_strchr_len(char *s, int c); 
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *str);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
