ad3d4400a803ae62c169cfb0aa166d0ff015c62e
AuroraSloan <64365011+AuroraSloan@users.noreply.github.com>
2021-02-10 00:36:20 +0900

    Update comp.sh

--- comp.sh
+++ comp.sh
@@ -2,7 +2,7 @@
 BUFF=32
 
 case ${1} in
-    "-f")	gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$BUFF get_next_line.c get_next_line_utils.c lblmain.c -o gnl 
+    "-ll")	gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$BUFF get_next_line.c get_next_line_utils.c lblmain.c -o gnl 
         ;;
     *)		gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$BUFF get_next_line.c get_next_line_utils.c fbfmain.c -o gnl 
 esac
